f0209d122c742cfecf1084a6a4a5e7244c003ce7
Ruslan Gorelov <lighttemper@gmail.com>
2020-05-12 22:29:09 +0700

    test ssh work

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	a := 1
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
