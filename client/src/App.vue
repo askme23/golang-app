f3ccf9eda91101ce98adac0750c5dc63420ff50a
Ruslan Gorelov <lighttemper@gmail.com>
2020-05-13 23:09:24 +0700

    front side and a little bit backend

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <div>
+      <b-avatar button v-b-toggle.sidebar-1 size="6rem" src="https://placekitten.com/300/300"></b-avatar>
+      Gorelov Ruslan
+      <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+        <div class="px-3 py-2">
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </p>
+          <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+        </div>
+      </b-sidebar>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Iosevka Sparkle Web', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
