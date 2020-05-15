5b41eff12b3f1acc25bf4d68748546e7a08ac011
Ruslan Gorelov <lighttemper@gmail.com>
2020-05-15 23:42:41 +0700

    just another commit )

--- api/controllers/login_controller.go
+++ api/controllers/login_controller.go
@@ -14,6 +14,7 @@ import (
 )
 
 func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+	// let's do it
 	w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
